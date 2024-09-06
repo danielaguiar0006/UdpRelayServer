6d28541a8760c3d49bf87203c4b6e8e8769b3dd2
danielaguiar0006 <danielaguiar0006@gmail.com>
2024-09-05 22:30:24 -0700

    Init UdpRelayServer Prototype

---
+++ Program.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Net;
+using System.Net.Sockets;
+
+public class UdpRelayServer
+{
+    public void StartRelayServer(int port)
+    {
+        UdpClient udpServer = new UdpClient(port);
+        udpServer.Client.Blocking = false;
+        Console.WriteLine($"Relay server started on port: {port}");
+
+        IPEndPoint clientEndPoint = new IPEndPoint(IPAddress.Any, 0);
+        while (true)
+        {
+            byte[] receivedData = udpServer.Receive(ref clientEndPoint);
+            udpServer.Send(receivedData, receivedData.Length, clientEndPoint);
+
+            string receivedMessage = System.Text.Encoding.UTF8.GetString(receivedData);
+            Console.WriteLine($"Received message from: {clientEndPoint.Address} on port: {clientEndPoint.Port}");
+            Console.WriteLine($"Received message: {receivedMessage}");
+
+            byte[] responseMessage = System.Text.Encoding.UTF8.GetBytes("Message received client! (This is server)");
+            udpServer.Send(responseMessage, responseMessage.Length, clientEndPoint);
+        }
+    }
+
+    public static void Main()
+    {
+        int port = 13439;
+
+        Console.WriteLine("Starting relay server...");
+        UdpRelayServer relayServer = new UdpRelayServer();
+        relayServer.StartRelayServer(port);
+    }
+}
